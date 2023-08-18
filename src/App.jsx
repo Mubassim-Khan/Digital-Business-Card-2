c9ac72ff7aba7d456f7c339dc1716377473bfb65
Mubassim-Khan <18924153-Mubassim-Khan@users.noreply.replit.com>
2023-08-18 11:02:10 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Info from './Components/Info'
+import About from './Components/About'
+import Interests from './Components/Interests'
+import Footer from './Components/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Info />
+      <About />
+      <Interests />
+      <Footer/>
+    </>
+  )
+}
---
+++ src/Components/About.jsx
@@ -0,0 +1,12 @@
+export default function About () {
+  return (
+<div className="container">
+
+    <div className="text-section">
+      <h4>About</h4>
+      <p>I am a Frontend Developer, having abilites & experince in tools, languages & frameworks like HTML 5, CSS, Tailwind CSS, JavaScript, React Js & more. I try to keep up with best techniques & practice. I'm always looking for new things to learn. </p>
+    </div>
+</div>
+
+    )
+}
